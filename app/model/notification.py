4aa4ea0c1485f2a96d69547033ba568b25beaf39
JohnGallegoZa <jgallego173@soyudemedellin.edu.co>
2026-04-30 16:30:12 -0500

    Se definieron las excepciones y se creó una clase abstracta con 3 metodos.

--- app/model/notification.py
+++ app/model/notification.py
@@ -1,6 +1,30 @@
-import os
 from dataclasses import dataclass, field
 from datetime import datetime
 from typing import Protocol, runtime_checkable
-
 from app.services.util import generate_unique_id
+import os
+from abc import ABC, abstractmethod
+
+class NotificationError(Exception):
+    pass
+
+class ChannelUnavailableError(NotificacionError):
+    pass
+
+class DeliveryError(NotificacionError):
+    pass
+
+class NotificationChannel(ABC):
+
+    @abstractmethod
+    def send(self, message: str) -> None:
+        pass
+
+    @abstractmethod
+    def get_channel_name(self) -> str:
+        pass
+
+    @abstractmethod
+    def is_available(self) -> bool:
+        pass
+
